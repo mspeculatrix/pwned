3d199dad205611992b02b8ece4b9bfe1efc25d75
mspeculatrix <smd@webvivant.com>
2019-06-18 16:56:08 +0200

    Update pwned.go

--- pwned.go
+++ pwned.go
@@ -5,8 +5,10 @@ Queries the haveibeenpwned.com API for breached passwords.
 The entered password is hashed with SHA1. The first five chars of the
 hash are sent to the API which returns the hashes of all the passwords
 whose first five chars match, along with a count of how many occurrences
-of that password are in the database. (The returned hashes are missing
-the first five chars because we already know those.)
+of that password are in the database. The hash and count are separated
+by a colon.
+The returned hashes are missing the first five chars because we already
+know those.
 The program then cycles through the returned hashes looking for that
 created by our password.
 
